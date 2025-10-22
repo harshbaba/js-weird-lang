c585b85dad7e1eea3d32085d20d527d3d5fed600
harshbaba <harshbaba007@gmail.com>
2025-10-22 19:48:24 +0530

    LTIMindTree Interview Experience

---
+++ interview-prepration/Interview_experience/LTI-MINDTree-22-OCT-2025.js
@@ -0,0 +1,46 @@
+===LTI-MINDTree-22-OCT-2025===
+
+1. Can you brifely tell me about your skill sets and projects where you have worked and about experience?
+2. How about your experience in cloud services, you have any experience?
+3. You have worked with ES6 and later versions so can you tell me what type of features you used in ypur project?
+4. Can you explain how event loop works in javascript?
+5. Do you know about WeakMap, when you will use that?
+6. Suppose you have to one ecommerce web app there you have to manage user authentications permissions and also 
+    you have to manage shopping cart and mini cart so how you will manage state in react. 
+    CanHow you will write architect for this using redux.
+
+    Basically in short: How you will create architecture of ecommerce web app in redux? Consider optimal performance & 
+    maintainability and also you can talk about challanges you might face and how you will solve those challanges.
+
+7. Lets say you have a legacy application that is built in react with class component and you have to migrate in functional component
+    Tell me approach how you will plan it like what are those areas to look for to complete the migration and complete the changes.
+
+8. Do you know about react fiber?
+9. You are working on react application and you have to optimise and enhancing the performance. So what are the steps you will do
+    to do that?
+=====Node Js===
+10. About Node Js can you tell me what kind of work you have done?
+11. Do you know about Event Emiiters? explain working.
+12. There is large text file in which all letters are in lower case, i need to read that file and then i need to convert 
+    all letters to upper case on that file. How we can do that.
+13. Tell me core modules of node js. When you have used which node modules.
+14. Whats the difference bw ProcessingInstruction.nextTick and setImmediate?
+15. How do you plan to write test cases in when you develop a new feature in react?
+16. What type of design patterns you use in your project, explain about situations when you had use them?
+
+
+async function foo() {
+  console.log("Start");
+  await delay(3000);
+  console.log("End");
+}
+//write delay method 
+function delay(ms) {
+
+    //return new Promise(resolve => setTimeout(resolve, ms));
+    // return new Promise((resolve) => {
+    //     setTimeout(() => {
+    //         resolve();
+    //     }, ms);
+    // }); 
+}  
